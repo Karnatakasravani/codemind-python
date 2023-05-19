e1578f761678b87ac034cc1a4f41e04a084fcb75
Karnatakasravani <119839721+Karnatakasravani@users.noreply.github.com>
2023-05-19 22:02:01 +0530

    def neon_number(s):     n=s*s     su=0     while n>0:         rem=n%10         su=su+rem         n=n//10     if su==s:         return 'Neon Number'     else:         return 'Not Neon Number' s=int(input()) print(neon_number(s))

---
+++ Neon_Number.py
@@ -0,0 +1,13 @@
+def neon_number(s):
+    n=s*s
+    su=0
+    while n>0:
+        rem=n%10
+        su=su+rem
+        n=n//10
+    if su==s:
+        return 'Neon Number' 
+    else:
+        return 'Not Neon Number'
+s=int(input())
+print(neon_number(s))
