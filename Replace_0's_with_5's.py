2d92c111b71ef46e7d07d10c25aa348e1fea3a98
Karnatakasravani <119839721+Karnatakasravani@users.noreply.github.com>
2023-10-29 07:50:03 +0530

    Create Replace_0's_with_5's.py

---
+++ Replace_0's_with_5's.py
@@ -0,0 +1,17 @@
+#function should return an integer
+#your task is to compplete this function
+class Solution:
+    def convertFive(self,n):
+        #Code here
+        return str(n).replace('0','5')
+
+#{ 
+ # Driver Code Starts
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n=int(input())
+        print(Solution().convertFive(n))
+# Contributed by: Harshit sidhwa
+
+# } Driver Code Ends
