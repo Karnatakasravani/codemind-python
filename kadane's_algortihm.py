dbf36091b11c87f18ab3c95bcfc79aa9a5591a16
Karnatakasravani <119839721+Karnatakasravani@users.noreply.github.com>
2023-11-04 05:46:49 +0530

    Create kadane's_algortihm.py

---
+++ kadane's_algortihm.py
@@ -0,0 +1,44 @@
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    #Function to find the sum of contiguous subarray with maximum sum.
+    def maxSubArraySum(self,arr,N):
+        ##Your code here
+        sum_values=0
+        max_sum=arr[0]
+        for i in range(N):
+            sum_values+=arr[i]
+            if max_sum<sum_values:
+                max_sum=sum_values
+            if sum_values<0:
+                sum_values=0
+        return max_sum
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+ 
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+            
+            arr=[int(x) for x in input().strip().split()]
+            
+            ob=Solution()
+            
+            print(ob.maxSubArraySum(arr,n))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
